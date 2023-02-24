0800bf59016b71a5fd65f598ef6fe9bb3653b280
ManYaiMark <105169896+ManYaiMark@users.noreply.github.com>
2023-02-25 02:12:26 +0700

    Update Tittle.py

--- project/Tittle.py
+++ project/Tittle.py
@@ -15,47 +15,40 @@ st.sidebar.title("Menu")
 select_box = st.sidebar.selectbox("Where are you going :",
                                   ('intro', "water potability", "Prediction Prediction", "about web"))
 if select_box == 'intro':
-    st.markdown(
-        f"""
-               <style>
-               .stApp {{
-                   background-image: url("https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80");
-                   background-attachment: fixed;
-                   background-size: cover;
-                   /* opacity: 0.3; */
-               }}
-               </style>
-               """,
-        unsafe_allow_html=True
-    )
-    #     text = "**Water potability refers to the suitability of water for consumption by humans without causing any adverse health effects. Potable water must meet certain standards and guidelines established by regulatory bodies such as the World Health Organization (WHO) and the Environmental Protection Agency (EPA) to ensure its safety.\n\nThe quality of water can be affected by various factors, including natural contaminants such as minerals and microorganisms, as well as man-made pollutants such as chemicals and industrial waste. The presence of harmful substances in water can cause health problems such as gastrointestinal illness, reproductive problems, and even cancer.**\n\n**To ensure water potability, water treatment processes are used to remove contaminants and ensure that the water is safe for consumption. These processes may include filtration, disinfection, and the addition of chemicals to remove impurities.**"
+        st.markdown(
+            f"""
+                   <style>
+                   .stApp {{
+                       background-image: url("https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80");
+                       background-attachment: fixed;
+                       background-size: cover;
+                       /* opacity: 0.3; */
+                   }}
+                   </style>
+                   """,
+            unsafe_allow_html=True
+        )
+        #     text = "**Water potability refers to the suitability of water for consumption by humans without causing any adverse health effects. Potable water must meet certain standards and guidelines established by regulatory bodies such as the World Health Organization (WHO) and the Environmental Protection Agency (EPA) to ensure its safety.\n\nThe quality of water can be affected by various factors, including natural contaminants such as minerals and microorganisms, as well as man-made pollutants such as chemicals and industrial waste. The presence of harmful substances in water can cause health problems such as gastrointestinal illness, reproductive problems, and even cancer.**\n\n**To ensure water potability, water treatment processes are used to remove contaminants and ensure that the water is safe for consumption. These processes may include filtration, disinfection, and the addition of chemicals to remove impurities.**"
 
-st.markdown("# Water Potability💧")
-st.write("**Water potability refers to the suitability of water for consumption by humans without causing any adverse health effects. Potable water must meet certain standards and guidelines established by regulatory bodies such as the World Health Organization (WHO) and the Environmental Protection Agency (EPA) to ensure its safety.**\n\n**The quality of water can be affected by various factors, including natural contaminants such as minerals and microorganisms, as well as man-made pollutants such as chemicals and industrial waste. The presence of harmful substances in water can cause health problems such as gastrointestinal illness, reproductive problems, and even cancer.**\n\n**To ensure water potability, water treatment processes are used to remove contaminants and ensure that the water is safe for consumption. These processes may include filtration, disinfection, and the addition of chemicals to remove impurities.**\n\n")
-data = pd.read_csv("./project/water_drop.csv")
-data = pd.DataFrame(data)
-x = data.drop(columns="Potability", axis=1)
-y = data["Potability"]
+    st.markdown("# Water Potability💧")
+    st.write("**Water potability refers to the suitability of water for consumption by humans without causing any adverse health effects. Potable water must meet certain standards and guidelines established by regulatory bodies such as the World Health Organization (WHO) and the Environmental Protection Agency (EPA) to ensure its safety.**\n\n**The quality of water can be affected by various factors, including natural contaminants such as minerals and microorganisms, as well as man-made pollutants such as chemicals and industrial waste. The presence of harmful substances in water can cause health problems such as gastrointestinal illness, reproductive problems, and even cancer.**\n\n**To ensure water potability, water treatment processes are used to remove contaminants and ensure that the water is safe for consumption. These processes may include filtration, disinfection, and the addition of chemicals to remove impurities.**\n\n")
+    data = pd.read_csv("./project/water_drop.csv")
+    data = pd.DataFrame(data)
+    x = data.drop(columns="Potability", axis=1)
+    y = data["Potability"]
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4)
 
-k_neighbors = np.arange(1, 51)
-train_score = np.empty(len(k_neighbors))
-test_score = np.empty(len(k_neighbors))
+    k_neighbors = np.arange(1, 51)
+    train_score = np.empty(len(k_neighbors))
+    test_score = np.empty(len(k_neighbors))
 
-for i, k in enumerate(k_neighbors):
-    knn = KNeighborsClassifier(n_neighbors=k)
-    knn.fit(x_train, y_train)
-    train_score[i] = knn.score(x_train, y_train)
-    test_score[i] = knn.score(x_test, y_test)
+    for i, k in enumerate(k_neighbors):
+        knn = KNeighborsClassifier(n_neighbors=k)
+        knn.fit(x_train, y_train)
+        train_score[i] = knn.score(x_train, y_train)
+        test_score[i] = knn.score(x_test, y_test)
 
-plt.title("Comare k value in model")
-plt.plot(k_neighbors, test_score, 'ro', label="Test score")
-plt.plot(k_neighbors, train_score, 'bo', label="Train score")
-plt.legend()
-plt.xlabel("K number")
-plt.ylabel("Score")
-plt.show()
 if select_box == "water potability":
     st.title("# Water Potability💧")
     wt.water()
