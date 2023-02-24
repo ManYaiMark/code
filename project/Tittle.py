136d1b68f25cc11c3f4e6cd84e241f4af23e4652
ManYaiMark <105169896+ManYaiMark@users.noreply.github.com>
2023-02-24 19:07:08 +0700

    Update Tittle.py

--- project/Tittle.py
+++ project/Tittle.py
@@ -1,20 +1,21 @@
-import water as wt
-import ussi_project as up
+# import water as wt
+# import ussi_project as up
 import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import time as t
 
+with st.spinner("loading...."):
+    t.sleep(4.1)
 st.sidebar.title("Menu")
-select_box=st.sidebar.selectbox("Where are you going :",('"-"',"water potability","Prediction Prediction"))
-# st.title(select_box)
-if select_box == '"-"':
-    # st.title("UUUU")
+select_box=st.sidebar.selectbox("Where are you going :",('intro',"water potability","Prediction Prediction","about web"))
+if select_box == 'intro':
     text = "**Water potability refers to the suitability of water for consumption by humans without causing any adverse health effects. Potable water must meet certain standards and guidelines established by regulatory bodies such as the World Health Organization (WHO) and the Environmental Protection Agency (EPA) to ensure its safety.**\n\n**The quality of water can be affected by various factors, including natural contaminants such as minerals and microorganisms, as well as man-made pollutants such as chemicals and industrial waste. The presence of harmful substances in water can cause health problems such as gastrointestinal illness, reproductive problems, and even cancer.**\n\n**To ensure water potability, water treatment processes are used to remove contaminants and ensure that the water is safe for consumption. These processes may include filtration, disinfection, and the addition of chemicals to remove impurities.**\n\n"
 
-    st.markdown("# Water Potability")
+    st.markdown("# Water Potability💧")
     st.write("**Water potability refers to the suitability of water for consumption by humans without causing any adverse health effects. Potable water must meet certain standards and guidelines established by regulatory bodies such as the World Health Organization (WHO) and the Environmental Protection Agency (EPA) to ensure its safety.**\n\n**The quality of water can be affected by various factors, including natural contaminants such as minerals and microorganisms, as well as man-made pollutants such as chemicals and industrial waste. The presence of harmful substances in water can cause health problems such as gastrointestinal illness, reproductive problems, and even cancer.**\n\n**To ensure water potability, water treatment processes are used to remove contaminants and ensure that the water is safe for consumption. These processes may include filtration, disinfection, and the addition of chemicals to remove impurities.**\n\n")
     data = pd.read_csv("./project/water_drop.csv")
     data = pd.DataFrame(data)
@@ -42,9 +43,12 @@ if select_box == '"-"':
     plt.ylabel("Score")
     plt.show()
 if select_box == "water potability":
-    # st.title(select_box)
+    st.title(select_box)
     wt.water()
 if select_box == "Prediction Prediction" :
-    # st.title(select_box)
+    st.title(select_box)
     up.population()
-# up.page()
+if select_box == "about web" :
+    st.title(select_box)
+    st.markdown("เว็ปนี้จัดทำขึ้นเพื่อศีกษาและหาข้อมูล streamlit ")
+    st.write("จัดทำโดย นายอัษฎาวุธ  ประสารคำ 65114540710")
